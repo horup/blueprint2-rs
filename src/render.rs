b11dfc5a40696c54645d60a0874380242d55b1ba
Horup <sah@sah.dk>
2021-01-03 18:35:28 +0100

    cleanup

--- src/render.rs
+++ src/render.rs
@@ -37,40 +37,7 @@ impl Render {
             panic!(gl.get_program_info_log(program));
         }
 
-      /*  let pos_loc = gl.get_attrib_location(program, "pos").expect("get_attrib_location failed");
-        gl.vertex_attrib_pointer_f32(0, 3, glow::FLOAT, false, std::mem::size_of::<Vertex>() as i32, 0);
-        gl.enable_vertex_attrib_array(0);
-        gl.vertex_attrib_pointer_f32(1, 2, glow::FLOAT, false, std::mem::size_of::<Vertex>() as i32, (std::mem::size_of::<u32>() * 3) as i32);
-        gl.enable_vertex_attrib_array(1);*/
-
         gl.use_program(Some(program));
-    }
-
-    unsafe fn setup_buffers(&mut self) {
-        /*let vertex_array = self.gl.create_vertex_array().expect("cannot create vertex array");
-        self.gl.bind_vertex_array(Some(vertex_array));
-
-        let position_buffer = self.gl.create_buffer().expect("failed");
-        self.gl.bind_buffer(glow::ARRAY_BUFFER, Some(position_buffer));
-            
-        let mut vertices = Vec::new();
-        // lower right triangle
-        vertices.push(Vertex::new(-0.5, -0.5, 0.0, 0.0, 0.0)); //1
-        vertices.push(Vertex::new(0.5, -0.5, 0.0, 1.0, 0.0)); //2
-        vertices.push(Vertex::new(0.5, 0.5, 0.0, 1.0, 1.0)); //3
-
-        // upper left triangle
-        vertices.push(Vertex::new(-0.5, -0.5, 0.0, 0.0, 0.0)); //1
-        vertices.push(Vertex::new(0.5, 0.5, 0.0, 1.0, 1.0)); //3
-        vertices.push(Vertex::new(-0.5, 0.5, 0.0, 0.0, 1.0)); //4
-
-
-
-        let buffer = std::slice::from_raw_parts(vertices.as_ptr() as *const u8, vertices.len() * std::mem::size_of::<Vertex>());
-        let buffer = vertices.align_to().1;        
-        log(&format!("{:?}", buffer));
-
-        self.gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, &buffer, glow::DYNAMIC_DRAW);*/
     }
 
     pub fn new(gl:Context) -> Self {
@@ -81,7 +48,6 @@ impl Render {
             meshes:Arena::new()
         };
         unsafe {
-            render.setup_buffers();
             render.setup_shaders();
         }
         return render;
@@ -121,16 +87,10 @@ impl Render {
             gl.clear_color(0.1, 0.2, 0.3, 1.0);
             gl.clear(glow::COLOR_BUFFER_BIT);
 
-            
-
             for (_, mesh) in &self.meshes {
                 gl.bind_vertex_array(Some(mesh.vertex_array_object));
                 gl.draw_arrays(glow::TRIANGLES, 0, mesh.vertices.len() as i32);
             }
-
-            let count = 1;
-            
-           // gl.draw_arrays(glow::TRIANGLES, 0, 6 * count);
         }
     }
 }
--- src/thing.rs
+++ src/thing.rs
@@ -1,3 +1,6 @@
+use nalgebra::Vector3;
+
 pub struct Thing {
-    
+    pub pos:Vector3<f32>
 }
+
