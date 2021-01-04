cd1a6a426a744d625a3bfc24aaee8776b99d4b91
Horup <sah@sah.dk>
2021-01-04 06:26:03 +0100

    refactor

--- src/mesh.rs
+++ src/mesh.rs
@@ -27,45 +27,39 @@ impl Mesh {
         return Self::new(&vertices, gl);
     }
 
-    pub fn new(vertices:&[Vertex], gl:&mut Context) -> Self {
-        unsafe {
-            let vertex_array_object = gl.create_vertex_array().expect("cannot create vertex array");
-            gl.bind_vertex_array(Some(vertex_array_object));
-            let vertex_buffer_object = gl.create_buffer().expect("failed");
-            gl.bind_buffer(glow::ARRAY_BUFFER, Some(vertex_buffer_object));
+    pub unsafe fn new(vertices:&[Vertex], gl:&mut Context) -> Self {
+        let vertex_array_object = gl.create_vertex_array().expect("cannot create vertex array");
+        gl.bind_vertex_array(Some(vertex_array_object));
+        let vertex_buffer_object = gl.create_buffer().expect("failed");
+        gl.bind_buffer(glow::ARRAY_BUFFER, Some(vertex_buffer_object));
 
-            gl.vertex_attrib_pointer_f32(0, 3, glow::FLOAT, false, std::mem::size_of::<Vertex>() as i32, 0);
-            gl.enable_vertex_attrib_array(0);
-            gl.vertex_attrib_pointer_f32(1, 2, glow::FLOAT, false, std::mem::size_of::<Vertex>() as i32, (std::mem::size_of::<u32>() * 3) as i32);
-            gl.enable_vertex_attrib_array(1);
+        gl.vertex_attrib_pointer_f32(0, 3, glow::FLOAT, false, std::mem::size_of::<Vertex>() as i32, 0);
+        gl.enable_vertex_attrib_array(0);
+        gl.vertex_attrib_pointer_f32(1, 2, glow::FLOAT, false, std::mem::size_of::<Vertex>() as i32, (std::mem::size_of::<u32>() * 3) as i32);
+        gl.enable_vertex_attrib_array(1);
 
-            let mut mesh = Self { 
-                vertices:vertices.to_vec(),
-                vertex_array_object,
-                vertex_buffer_object
-            };
+        let mut mesh = Self { 
+            vertices:vertices.to_vec(),
+            vertex_array_object,
+            vertex_buffer_object
+        };
 
-            mesh.update(gl);
+        mesh.update(gl);
 
-            return mesh;
-        }
+        return mesh;
     }
 
-    pub fn update(&self, gl:&Context) {
-        unsafe {
-            let buffer = std::slice::from_raw_parts(self.vertices.as_ptr() as *const u8, self.vertices.len() * std::mem::size_of::<Vertex>());
-            let buffer = buffer.align_to().1;        
-            //log(&format!("{:?}", buffer));
+    pub unsafe fn update(&self, gl:&Context) {
+        let buffer = std::slice::from_raw_parts(self.vertices.as_ptr() as *const u8, self.vertices.len() * std::mem::size_of::<Vertex>());
+        let buffer = buffer.align_to().1;        
+        //log(&format!("{:?}", buffer));
 
-            gl.bind_buffer(glow::ARRAY_BUFFER, Some(self.vertex_buffer_object));
-            gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, &buffer, glow::DYNAMIC_DRAW);
-        }
+        gl.bind_buffer(glow::ARRAY_BUFFER, Some(self.vertex_buffer_object));
+        gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, &buffer, glow::DYNAMIC_DRAW);
     }
 
-    pub fn draw(&self, gl:&Context) {
-        unsafe {
-            gl.bind_vertex_array(Some(self.vertex_array_object));
-            gl.draw_arrays(glow::TRIANGLES, 0, self.vertices.len() as i32);
-        }
+    pub unsafe fn draw(&self, gl:&Context) {
+        gl.bind_vertex_array(Some(self.vertex_array_object));
+        gl.draw_arrays(glow::TRIANGLES, 0, self.vertices.len() as i32);
     }
 }
