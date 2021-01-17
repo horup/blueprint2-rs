5be889fe3428c412db9bf5d5762d12b2cac0cb5c
Horup <sah@sah.dk>
2021-01-17 08:16:10 +0100

    added movement yet again

--- src/game.rs
+++ src/game.rs
@@ -13,10 +13,10 @@ impl Game for BlueprintGame {
     type GameComponent1 = ();
 
     fn update(&mut self, context:&mut Context<Self>) {
+        let current = context.states.current_mut();
         match context.event {
             Event::Initialize => {
                 log("Game initialized");
-                let current = context.states.current_mut();
                 let mut assets = &mut context.assets;
                 let sheet01 = assets.load_texture_from_png_bytes("sheet01".into(), include_bytes!("./assets/textures/spritesheet.png"));
                 
@@ -40,9 +40,9 @@ impl Game for BlueprintGame {
                
             }
             Event::FixedStep(time, dt) => {
-               /* for (_, t) in context.current.entities.iter_mut() {
-                    t.pos.x += 0.1 * dt as f32;
-                }*/
+                for (_, t) in current.entities.query_mut::<&mut Transform>() {
+                    t.position.x += 0.1 * dt as f32;
+                }
             }
             Event::Draw(_,_,_) => {
                
