f94936c8d17c421f3410bdd1231a7ff75a387eea
Horup <sah@sah.dk>
2021-01-24 22:13:11 +0100

    fixed noisy exception

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-import('./pkg').catch(console.error);
+import('./pkg').catch(e=>{
+    if (e.message != "Using exceptions for control flow, don't mind me. This isn't actually an error!") {
+        console.error(e.message);
+    }
+});
