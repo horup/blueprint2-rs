da698c7a0512e62360367059c2964dd8f8bbf0c6
horup <sah@sah.dk>
2021-02-10 16:10:50 +0100

    worked a bit on systems abstraction

--- engine/src/system.rs
+++ engine/src/system.rs
@@ -1,6 +1,48 @@
-use crate::{Event, Game};
+use core::panic;
+use std::{any::{Any, TypeId}, marker::PhantomData};
+
+use crate::{Event, Game, log};
 use crate::Context;
 pub trait System<S:Game> {
     fn update(&mut self, event:&Event<S>, context:&mut Context<S>) {
+    }
+}
+
+#[derive(Default)]
+pub struct Systems<G:Game> { 
+    systems:Vec<Box<dyn Any>>,
+    marker:PhantomData<G>
+}
+
+impl<G:Game + 'static> Systems<G> {
+    pub fn add<T:System<G> + Default + 'static>(&mut self) -> bool {
+        let t_id = TypeId::of::<Box<T>>();
+        log(&format!("add {:?}", t_id));
+
+        if self.has::<T>() == false {
+            self.systems.push(Box::new(T::default()));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    pub fn has<T:System<G> + 'static>(&self) -> bool {
+        for system in &self.systems {
+            let any = system.as_ref();
+            if let Some(as_t) = any.downcast_ref::<T>() {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    pub fn get_mut<T:System<G> + 'static>(&mut self) -> &mut T {
+        let my_id = TypeId::of::<T>();
+        for system in &mut self.systems {
+            
+        }
+        panic!();
     }
 }
